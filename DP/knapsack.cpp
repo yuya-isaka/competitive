d1da4007a90cd054c3f5af10d24588a057c25128
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2020-02-18 02:05:29 +0900

    ナップサック問題

---
+++ DP/knapsack.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <set>
+#include <utility>
+#include <cstdlib>
+#include <queue>
+#include <stack>
+#include <iomanip>
+#include <cstdio>
+#include <map>
+#include <list>
+#include <stdio.h>
+#include <sstream>
+using namespace std;
+using ll = long long;
+using P = pair<ll,ll>;
+#define all(x) (x).begin(), (x).end()
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() );
+//ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
+//ll lcm(ll x, ll y) {return x / gcd(x, y) * y;}
+const int dx[4] = {1,0,-1,0};
+const int dy[4] = {0,1,0,-1};
+const ll mod = 1e9 + 7;
+const ll inf = 1LL << 60;
+const long double pi = 3.14159265358979323846;
+
+// ****************************************CODE***************************************//
+
+ll n, w, weight[110], value[110];
+ll dp[110][100100];
+
+int main() {
+    cin >> n >> w;
+    for(int i = 0; i < n; i++) cin >> weight[i] >> value[i];
+    
+
+    for(int i = 0; i < n; i++){
+        for(int sum_w = 0; sum_w <= w; sum_w++){
+
+            if(sum_w - weight[i] >= 0) {
+                dp[i+1][sum_w] = max(dp[i+1][sum_w], dp[i][sum_w - weight[i]] + value[i]);
+            }
+            dp[i+1][sum_w] = max(dp[i+1][sum_w], dp[i][sum_w]);
+
+        }
+    }
+
+    cout << dp[n][w] << endl;
+}
