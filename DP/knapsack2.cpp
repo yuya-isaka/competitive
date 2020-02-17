05869ccba0d3fa2f9a4778cf67bd7f89f9c75061
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2020-02-18 01:50:21 +0900

    knapsack2, 荷物の重さの制約が厳しいバージョン

---
+++ DP/knapsack2.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <set>
+#include <utility>
+#include <cstdlib>
+#include <queue>
+#include <stack>
+#include <iomanip>
+#include <cstdio>
+#include <map>
+#include <list>
+#include <stdio.h>
+#include <sstream>
+using namespace std;
+using ll = long long;
+using P = pair<ll,ll>;
+#define _upgrade ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() );
+#define pb push_back
+//ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
+//ll lcm(ll x, ll y) {return x / gcd(x, y) * y;}
+const int dx[4] = {1,0,-1,0};
+const int dy[4] = {0,1,0,-1};
+const ll mod = 1e9 + 7;
+const ll inf = 1LL << 60;
+const long double pi = 3.14159265358979323846;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+// ****************************************CODE***************************************//
+
+ll n, w, weight[110], value[110];
+ll dp[110][100100];
+
+int main() {
+    cin >> n >> w;
+    for(int i = 0; i < n; i++){
+        cin >> weight[i] >> value[i];
+    }
+
+    for(int i = 0; i < 110; i++) for(int j = 0; j < 100100; j++) dp[i][j] = inf;
+
+    dp[0][0] = 0;
+
+    for(int i = 0; i < n; i++){
+        for(int sum_v = 0; sum_v < 100100; sum_v++) {
+
+            if(sum_v - value[i] >= 0) dp[i+1][sum_v] = min(dp[i+1][sum_v], dp[i][sum_v - value[i]] + weight[i]);
+
+            dp[i+1][sum_v] = min(dp[i+1][sum_v], dp[i][sum_v]);
+        }
+    }
+
+    ll res = 0;
+    for(int sum_v = 0; sum_v < 100100; sum_v++){
+        if(dp[n][sum_v] <= w) res = sum_v;
+    }
+
+    cout << res << endl;
+    return 0;
+}
