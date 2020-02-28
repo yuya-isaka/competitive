60b5a75812c97396d430ccd68d40feea7949e66d
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2020-02-28 18:05:20 +0900

    python_first_frog1

--- backet_python/kodogiasu.py
+++ backet_python/kodogiasu.py
@@ -1,11 +1,12 @@
 import sys
 import itertools
 from operator import itemgetter #sortedの対象を決めたい
-from fractions import gcd #最大公約数求めたい
-from math import ceil, floor, sqrt #小数点切り上げ、切り捨て、平方根求めたい
-from copy import deepcopy #参照で影響されないコピーが作られる
-from collections import Counter, deque #要素ごとの出現回数を数える。双方向アクセス可能データ型
+from fractions import gcd #最大公約数
+from math import ceil, floor, sqrt, isinf #小数点切り上げ、切り捨て、平方根
+from copy import deepcopy #参照で影響されないコピー
+from collections import Counter, deque #要素ごとの出現回数、双方向アクセス可能データ型
 import heapq
+import numpy as np
 from functools import reduce
 sys.setrecursionlimit(200000)
 
@@ -13,30 +14,16 @@ input = sys.stdin.readline
 # template
 
 
-p = int(input())
-a = list(map(int,input().split()))
+n = int(input())
+h = list(map(int, input().split()))
 
-b = list(set(a))
-n = len(b) #種類の数
+dp = [1e9] * n
 
-s = 0
-t = 0
-num = 0
-ass = p
-count = [0 for i in range(max(a)+2)] #count[i]:iの出現数
+dp[0] = 0
 
-while True:
-    while t < p and num < n:
-        if count[a[t]] == 0:
-            num += 1
-        count[a[t]] += 1
-        t += 1
-    if num < n:
-        break
-    ass = min(ass,t-s)
-    if count[a[s]]-1 == 0:
-        num -=1
-    count[a[s]] -=1
-    s += 1
-print(ass)
+for i in range(1, n):
+    dp[i] = min(dp[i], dp[i-1] + abs(h[i] - h[i-1]))
+    if(i > 1):
+         dp[i] = min(dp[i], dp[i-2] + abs(h[i] - h[i-2]))
 
+print(dp[-1])
