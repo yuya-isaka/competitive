1e7e289954fb383e1c3a11911e78106c52fa41bb
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2020-03-02 10:46:16 +0900

    frog1

---
+++ backet_python/sakurasou.py
@@ -0,0 +1,13 @@
+n = int(input())
+h = [int(i) for i in input().split()]
+
+dp = [float('inf')] * n
+
+dp[0] = 0
+
+for i in range(1, n):
+    dp[i] = min(dp[i], dp[i-1] + abs(h[i] - h[i-1]))
+    if i > 0:
+        dp[i] = min(dp[i], dp[i-2] + abs(h[i] - h[i-2]))
+
+print(dp[-1])
