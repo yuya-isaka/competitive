889092007e5f530a3e7ecf0bb9772a9c1becad34
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2020-03-07 01:33:48 +0900

    yukicoder solution

--- contest/yukicoder/b.py
+++ contest/yukicoder/b.py
@@ -1,40 +1,49 @@
-mod = 2**32
-
+# 解説を見たらわかるけど
+# つまり、交互に偶数と奇数が出ることになることに気付けるかがポイント　これは制約をしっかり見ないと気付けない。　
 a, b, x, n = map(int, input().split())
 
-listlist = [0] * (n*2)
+if x % 2 == 0:
+    print(0, n//2)
+else:
+    print(n//2, 0)
 
-for i in range(0, len(listlist)):
-    tmp = a * x % mod
-    tmp = (tmp + b) % mod
-    x = tmp
-    listlist[i] = tmp
+# mod = 2**32
 
-taka = []
-aoki = []
-for i in range(len(listlist)):
-    flag = True
-    if listlist[i] % 2 == 0:
-        flag = True
-    else: 
-        flag = False
+# a, b, x, n = map(int, input().split())
 
-    if i % 2 == 0:
-        if flag:
-            taka.append(1)
-        else:
-            taka.append(0)
+# listlist = [0] * (n*2)
 
-    else:
-        if flag:
-            aoki.append(1)
-        else:
-            aoki.append(0)
+# for i in range(0, len(listlist)):
+#     tmp = a * x % mod
+#     tmp = (tmp + b) % mod
+#     x = tmp
+#     listlist[i] = tmp
 
-takacount = str(min(taka.count(1), taka.count(0)))
-aoki = str(min(aoki.count(1), aoki.count(0)))
+# taka = []
+# aoki = []
+# for i in range(len(listlist)):
+#     flag = True
+#     if listlist[i] % 2 == 0:
+#         flag = True
+#     else: 
+#         flag = False
 
-print(takacount + ' ' + aoki)
+#     if i % 2 == 0:
+#         if flag:
+#             taka.append(1)
+#         else:
+#             taka.append(0)
+
+#     else:
+#         if flag:
+#             aoki.append(1)
+#         else:
+#             aoki.append(0)
+
+# takacount = str(min(taka.count(1), taka.count(0)))
+# aoki = str(min(aoki.count(1), aoki.count(0)))
+
+# print(takacount + ' ' + aoki)
 
 #純粋に、その入力分を調べていく
 #まずはどんな数列が出るか調べて、それを試して行った
--- contest/yukicoder/c.py
+++ contest/yukicoder/c.py
@@ -1,27 +1,50 @@
+# これは、場合わけをしっかり考えれるかが大事だった。　一文字のとき分けれないんだなーと気付けること。そしてあとは実行時間に間に合うレベルの全探索だということに。
 s = input()
 t = input()
 
+length = len(t)
+
+if length == 1 and t in s:
+    print(-1)
+    exit(0)
+
 ans = 0
-j = 0
-i = 0
-while i != len(s):
-    if j == len(t):
+k = 0
+while length + k <= len(s):
+    if s[k:k+length] == t:
         ans += 1
-        i -= 1
-        j = 0
-        continue
-
-    if s[i] == t[j]:
-        j += 1
+        k += length -1   #ここで最後の一文字をしっかりもう一度見るということを実装するのが大事
     else:
-        j = 0
-
-    i += 1
-
-if j == len(t):
-    ans += 1
+        k += 1
 
 print(ans)
+
+
+
+# s = input()
+# t = input()
+
+# ans = 0
+# j = 0
+# i = 0
+# while i != len(s):
+#     if j == len(t):
+#         ans += 1
+#         i -= 1
+#         j = 0
+#         continue
+
+#     if s[i] == t[j]:
+#         j += 1
+#     else:
+#         j = 0
+
+#     i += 1
+
+# if j == len(t):
+#     ans += 1
+
+# print(ans)
 
 
 #繰り返し長さ分を測って、一緒だったらcheckを増やして規定に達したら答えにプラスしていく方式を試した　ダメだった
